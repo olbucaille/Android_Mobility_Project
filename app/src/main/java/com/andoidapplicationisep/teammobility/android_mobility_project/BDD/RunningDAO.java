db08525bcc5111c097c5d05961405450418f1caf
gsurget <surgetg@gmail.com>
2016-01-02 12:33:26 +0100

    essai remplissage bdd running 2

--- app/src/main/java/com/andoidapplicationisep/teammobility/android_mobility_project/BDD/RunningDAO.java
+++ app/src/main/java/com/andoidapplicationisep/teammobility/android_mobility_project/BDD/RunningDAO.java
@@ -2,6 +2,10 @@ package com.andoidapplicationisep.teammobility.android_mobility_project.BDD;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by apple on 25/11/2015.
@@ -57,5 +61,17 @@ public class RunningDAO extends DAOBase {
         // CODE
         return new Running();
     }
+    public ArrayList<Activity> getRunning (String activityID) {
+        ArrayList<Activity> activityArrayList = new ArrayList<Activity>();
 
+        Cursor cursor = mDb.rawQuery("select " + "*" + " from " + RUNNING_TABLE_NAME + " where " + RUNNING_ACTIVITY_ID + "= ? ", new String[]{activityID});
+        while (cursor.moveToNext()) {
+            long id = cursor.getLong(0);
+            String activityId = cursor.getString(1);
+            int distance = cursor.getInt(2);
+            Log.d("activityId", activityId);
+        }
+        cursor.close();
+        return activityArrayList;
+    }
 }
